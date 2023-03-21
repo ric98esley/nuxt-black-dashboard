f87516ee89740d14ef5684b326474d9a1c330fab
Ricardo Perez <ricardo.pereze.98@gmail.com>
2023-03-20 22:23:34 -0400

    [UPDATE] Nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -88,6 +88,11 @@ export default {
     langDir: "lang/",
     defaultLocale: "en",
   },
+
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost
+  },
   /*
    ** Build configuration
    */
