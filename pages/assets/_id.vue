8fb589725c43e41a35da1e8c70da11325a6dde66
Ricardo Perez <ricardo.pereze.98@gmail.com>
2023-05-24 20:15:58 -0400

    [UPDATE] asset view

--- pages/assets/_id.vue
+++ pages/assets/_id.vue
@@ -7,45 +7,55 @@
           <div class="row">
             <div class="col-md-6">
               <label>Serial:</label>
-              {{ asset.serial }}
+              {{ asset?.serial }}
             </div>
             <div class="col-md-6">
               <label> Creado por:</label>
-              {{ asset.createdBy?.username }}
+              {{ asset?.createdBy?.username }}
             </div>
             <div class="col-md-6">
               <label> Fecha de creación</label>
-              {{ formatDate(asset.createdAt) }}
+              {{ formatDate(asset?.createdAt) }}
             </div>
             <div class="col-md-6">
               <label> Número de entradas</label>
-              {{ asset.countChecking }}
+              {{ asset?.countChecking }}
             </div>
             <div class="col-md-6">
               <label> Modelo</label>
-              {{ asset.model?.model }}
+              {{ asset?.model?.model }}
             </div>
             <div class="col-md-6">
               <label> Categoria</label>
-              {{ asset.model?.category.name }}
+              {{ asset?.model?.category.name }}
             </div>
             <div class="col-md-6">
               <label> Estado</label>
-              {{ asset.state?.name }}
+              {{ asset?.state?.name }}
             </div>
           </div>
         </form>
+        <base-button @click="modals.updateAsset = true">
+          Editar activo
+        </base-button>
       </card>
     </div>
     <div class="col-md-6">
       <card>
-        <base-button> editar activo </base-button>
+        <div id="barcode" @click="print">
+          <barcode
+            :value="asset?.serial"
+            format="CODE128"
+            width="1"
+            height="30"
+          ></barcode>
+        </div>
       </card>
     </div>
     <div class="col-md-6">
       <card>
         <h4>Historial de asignaciones</h4>
-        <el-table :data="asset.assignments">
+        <el-table :data="asset?.assignments">
           <el-table-column type="expand">
             <template slot-scope="{ row }">
               <div class="row">
@@ -89,11 +99,81 @@
       <card>
         <h4>Historial mantenimientos</h4>
         <el-table></el-table>
-        <div id="barcode">
-          <barcode :value="asset.serial" format="CODE128" width="1" height="30"></barcode>
-        </div>
-        <base-button @click="print"> imprimir codigo de barras </base-button>
       </card>
+    </div>
+    <!-- Modals -->
+    <div>
+      <modal
+        :show.sync="modals.updateAsset"
+        body-classes="p-0"
+        modal-classes="modal-dialog-centered"
+      >
+        <card class="p-3">
+          <h5 slot="header" class="title">Editar activo</h5>
+          <form @submit.prevent="updateAsset">
+            <div class="row">
+              <div class="col-md-12">
+                <base-input
+                  type="text"
+                  label="Serial"
+                  v-model="asset.serial"
+                  disabled
+                >
+                </base-input>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                <base-input type="text" label="Modelo">
+                  <el-select
+                    v-model="toUpdate.modelId"
+                    filterable
+                    class="select-success"
+                    placeholder="Selecciona una categoria"
+                    label="Categoria"
+                    style="width: 100%"
+                  >
+                    <el-option
+                      v-for="option in models"
+                      :key="option.id"
+                      :value="option.id"
+                      :label="`${option.category.name} - ${option.name} - ${option.brand?.name}`"
+                    >
+                    </el-option>
+                  </el-select>
+                </base-input>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                <base-input type="text" label="Estado">
+                  <el-select
+                    v-model="toUpdate.assetStateId"
+                    class="select-success"
+                    placeholder="Selecciona un estado"
+                    label="Categoria"
+                    style="width: 100%"
+                  >
+                    <el-option
+                      v-for="option in states"
+                      :key="option.id"
+                      :value="option.id"
+                      :label="option.name"
+                    >
+                    </el-option>
+                  </el-select>
+                </base-input>
+              </div>
+            </div>
+
+            <base-button native-type="submit" type="primary" class="btn-fill">
+              Crear activo
+            </base-button>
+          </form>
+        </card>
+      </modal>
     </div>
   </div>
 </template>
@@ -101,6 +181,7 @@
 <script>
 import { Select, Option, Table, TableColumn, Pagination } from "element-ui";
 import VueBarcode from "vue-barcode";
+import { BaseSwitch, Modal } from "@/components";
 
 export default {
   name: "AssetDetails",
@@ -112,19 +193,29 @@ export default {
     [TableColumn.name]: TableColumn,
     [Pagination.name]: Pagination,
     barcode: VueBarcode,
+    Modal,
   },
   data() {
     return {
       asset: {},
       id: this.$route.params.id,
+      modals: {
+        updateAsset: false,
+      },
+      toUpdate: {
+      },
+      states: [],
+      models: [],
     };
   },
   mounted() {
+    this.getStatus();
+    this.getModels();
     this.getAsset();
   },
   methods: {
     async print() {
-      console.log(this.$Printd.print(document.getElementById('barcode')));
+      console.log(this.$Printd.print(document.getElementById("barcode")));
     },
     formatDate(dateString) {
       // Crear un objeto de fecha a partir de la cadena de fecha
@@ -145,9 +236,41 @@ export default {
       return fechaFormateada;
     },
     async getAsset() {
-      const { data } = await this.$axios.get(`/assets/${this.id}`);
+      const { data } = await this.$axios.get(
+        `/assets/${this.id}`
+      );
       this.asset = data;
-      console.log(this.asset);
+    },
+    async getStatus() {
+      try {
+        const { data, error } = await this.$axios.get("/assets/status");
+        this.states = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getModels() {
+      try {
+        const { data, error } = await this.$axios.get("/assets/models");
+        this.models = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async updateAsset() {
+      try {
+        const toSend = {...this.toUpdate}
+        const { data, error } = await this.$axios.patch(`/assets/${this.id}`, toSend);
+        this.$notify({
+          message: `activo actualizado correctamente`,
+          type: "success",
+        })
+        this.toUpdate = {}
+        this.getAsset();
+        this.modals.updateAsset = false
+      } catch (error) {
+        console.log(error)
+      }
     },
   },
 };
